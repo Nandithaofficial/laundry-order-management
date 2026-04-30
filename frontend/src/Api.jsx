182af0abce59cf9854ce491759fb7c878b482b39
Nanditha <nanditha.kallu2023@gmail.com>
2026-04-30 09:51:58 +0530

    Update API URL for production

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://laundry-order-management-657i.onrender.com/api",
 });
 
 export const createOrder = (data) => API.post("/orders", data);
